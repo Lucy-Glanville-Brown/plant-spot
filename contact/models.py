734dc6c19980dad74267a83ed1a49af8f5d4954e
Lucy-Glanville-Brown <lucyglanvillebrown@gmail.com>
2022-06-27 10:52:50 +0000

    Added model for contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    """ Model to record the contact form """
+    name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    subject = models.CharField(max_length=100, null=False, blank=False)
+    message = models.CharField(max_length=300, null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name + "-" + self.subject

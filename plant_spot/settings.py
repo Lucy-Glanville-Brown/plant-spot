20e1357753dbbe77c14c3476df9a5dc2e7e521c8
Lucy-Glanville-Brown <lucyglanvillebrown@gmail.com>
2023-09-15 17:36:55 +0000

    add build.sh and settings for render deployment

--- plant_spot/settings.py
+++ plant_spot/settings.py
@@ -32,6 +32,12 @@ DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ['plant-spot.herokuapp.com', 'localhost']
 
+ # Add Render.com URL to allowed hosts
+ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+ if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
 
 # Application definition
 
